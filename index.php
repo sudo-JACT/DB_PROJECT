0c9243c6cfc0ee507c56459f42f9cc1ed25c3e0a
1MD0NK3Y <jacopino.toma@gmail.com>
2026-08-04 10:11:11 +0200

    Finito filtri
    Messi tutti i filtri

--- index.php
+++ index.php
@@ -2,7 +2,39 @@
 
     include_once("./php/template.php");
 
-    session_checker()
+    session_checker();
+
+
+    if (isset($_POST['orderby'])) {
+
+        switch ($_POST['orderby']) {
+
+            case 'A-Z':
+
+                $sql = "SELECT a.id as id, a.name as name, a.image_path as image_path, b.name as bname, a.price as price FROM album as a join published as p on p.album_id=a.id join band as b on b.id=p.band_id ORDER BY a.name";
+                break;
+
+            case 'Z-A':
+
+                $sql = "SELECT a.id as id, a.name as name, a.image_path as image_path, b.name as bname, a.price as price FROM album as a join published as p on p.album_id=a.id join band as b on b.id=p.band_id ORDER BY a.name DESC";
+                break;
+
+            case 'Ascending':
+
+                $sql = "SELECT a.id as id, a.name as name, a.image_path as image_path, b.name as bname, a.price as price FROM album as a join published as p on p.album_id=a.id join band as b on b.id=p.band_id ORDER BY a.price";
+                break;
+            
+            case 'Discending':
+
+                $sql = "SELECT a.id as id, a.name as name, a.image_path as image_path, b.name as bname, a.price as price FROM album as a join published as p on p.album_id=a.id join band as b on b.id=p.band_id ORDER BY a.price DESC";                break;
+        }
+
+    } else {
+
+
+        $sql = "SELECT a.id as id, a.name as name, a.image_path as image_path, b.name as bname, a.price as price FROM album as a join published as p on p.album_id=a.id join band as b on b.id=p.band_id";
+
+    }
 
 ?>
 
@@ -22,26 +54,38 @@
             navbar(true);
 
         ?>
-    
-        <div class="title">
-     
-            <h1><?php echo $_SESSION['title'] ?></h1>
 
-            <form method='POST'>
-           
-                <select class="form-select form-select-lg mb-3 card-dark text-neon" name='orderby'>
-                    <option disabled selected value=''>Order By</option> 
-                    <option><button type='submit' class='btn'>None</button></option>
-                    <option><button type='submit' class='btn'>A-Z</button></option>
-                    <option><button type='submit' class='btn'>Price ascending</button></option>
-                    <option><button type='submit' class='btn'>Price descending</button></option>
-                </select>
+        <div class='filters_div'>
+
+            <div class="title">
         
+                <h1><?php echo "WELCOME TO " . $_SESSION['title'] . "!!!" ?></h1>
+
+            </div>
+
+            <form method='POST' class='filters'>
+
+                <div class="btn btn-group">
+                    
+                    <button class="filter-btn btn btn-secondary btn-sm dropdown-toggle" type="button" data-bs-toggle="dropdown" aria-expanded="false">
+                        Order By
+                    </button>
+                    
+                    <ul class="dropdown-menu filter">
+                        <button type='submit' class='btn' name='orderby' value='A-Z'><li class='filt'>A-Z</li></button>
+                        <div class='border-top'></div>
+                        <button type='submit' class='btn' name='orderby' value='Z-A'><li class='filt'>Z-A</li></button>
+                        <div class='border-top'></div>
+                        <button type='submit' class='btn' name='orderby' value='Ascending'><li class='filt'>Ascending Price</li></button>
+                        <div class='border-top'></div>
+                        <button type='submit' class='btn' name='orderby' value='Discending'><li class='filt'>Discending Price</li></button>
+                    </ul>
+                
+                </div> 
 
             </form>
      
         </div>
-
 
         <form method="POST" action='/php/product_page.php'>
 
@@ -61,7 +105,6 @@
 
                 try {
 
-                    $sql = "SELECT a.id as id, a.name as name, a.image_path as image_path, b.name as bname, a.price as price FROM album as a join published as p on p.album_id=a.id join band as b on b.id=p.band_id";
                 
                     $result = $conn->query($sql);
                 
@@ -70,7 +113,6 @@
                     
                         while($row = $result->fetch()) {
                             
-                        /*echo "<div class='card card-dark' style='width: 18rem;'>*/
                         echo "<div class='card card-dark'>
                                 <button class='btn' type='submit' value='".$row['id']."' name='productid'><img src='".$row['image_path']."' class='card-img-top' alt='".$row['name']."'>
                                 </button>
@@ -80,18 +122,6 @@
                                     <p class='card-text pricetag'> ".roundPrice($row['price'])." € </p>
                                 </div>
                             </div>";
-                        /*
-                            echo "<div class='album foto'>"; 
-                            echo "<figure class='figure border-neon'>";
-                            echo "<img src='".$row['image_path']."' class='figure-img img-fluid rounded' alt=".$row['name'].">";
-                            // nome album
-                            echo "<figcaption class='figure-caption text-center text-neon-w'>".$row['bname']."</figcaption>";
-                            echo "</figure>";
-                            echo "<br/>";
-                            // nome della band
-                            echo "<text class='text-neon'>".$row['name']."</text>";
-                            echo "</div>";
-                        */
                         }
                     
                         unset($result);
