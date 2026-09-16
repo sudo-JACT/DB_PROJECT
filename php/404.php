47da06fa85e2b9a898ce08ae0375755478661105
brebreee87 <mr.cheekbones444@gmail.com>
2026-09-16 20:42:42 +0200

    commento file

--- php/404.php
+++ php/404.php
@@ -1,10 +1,10 @@
 <?php
 
-    include_once("./template.php");
-    session_checker();
+    include_once("./template.php"); 
+    session_checker(); // controllo sessione
 ?>
 
-
+// inclusione di head, navbar e codice errore
 <html lang="en">
 
     <?php
@@ -26,9 +26,9 @@
 
         <?php
 
-            http_response_code(404);
+            http_response_code(404); // http
 
-            echo "<div class='title'>";
+            echo "<div class='title'>"; // messaggio di errore all'interno della pagina
             echo "<h1>404</h1>";
             echo "</div>";
 
